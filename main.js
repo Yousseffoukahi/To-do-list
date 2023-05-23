7457b48f0739ca35aa4b7ac01cb65cb0ea12ecc8
Yousseffoukahi <126264747+Yousseffoukahi@users.noreply.github.com>
2023-05-23 14:29:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let inp = document.getElementById.trim()('inp');
+let plus = document.getElementById('ha');
+let delet =document.getElementById('delet');
+let check = document.getElementById('check');
+let task = document.getElementsByClassName('task');
+let inp2 = document.getElementById('inp2')
+let h1 = document.getElementById('h1')
+ let x=inp.Value()
+plus.onclick=function(){
+   task.h1='<h1>x</h1>';
+    if(x!=""){
+        
+    }
+    else{
+        
+    }
+   
+}
